56be770c0687b4f59bfe804504614f41d0b6802f
Arnold Vink <info@arnoldvink.com>
2025-06-12 10:28:54 +0200

    - Check if tuning service is available.

--- AmdDriverTool/AdlxValuesReset.h
+++ AmdDriverTool/AdlxValuesReset.h
@@ -5,19 +5,30 @@
 
 namespace winrt::AmdDriverTool::implementation
 {
-	void MainPage::AdlxValuesReset()
+	bool MainPage::AdlxValuesReset()
 	{
+		//Check if variable is available
+		if (ppGPUTuningServices == nullptr)
+		{
+			//Set result
+			textblock_Status().Text(L"Tuning and fans not reset");
+			AVDebugWriteLine(L"Tuning and fans not reset");
+			return false;
+		}
+
 		//Reset gpu tuning and fan settings
 		adlx_Res0 = ppGPUTuningServices->ResetToFactory(ppGpuInfo);
 		if (ADLX_FAILED(adlx_Res0))
 		{
 			textblock_Status().Text(L"Tuning and fans reset failed");
 			AVDebugWriteLine(L"Tuning and fans reset failed");
+			return false;
 		}
 		else
 		{
 			textblock_Status().Text(L"Tuning and fans reset");
 			AVDebugWriteLine(L"Tuning and fans reset");
+			return true;
 		}
 	}
 }
