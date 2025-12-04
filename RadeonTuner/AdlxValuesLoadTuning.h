8d0ff8d8ad22f6df2700120c03f057e9825a11fa
Arnold Vink <info@arnoldvink.com>
2025-12-04 15:18:47 +0100

    - Disable tuning interface when value is not read.

--- RadeonTuner/AdlxValuesLoadTuning.h
+++ RadeonTuner/AdlxValuesLoadTuning.h
@@ -33,34 +33,63 @@ namespace winrt::RadeonTuner::implementation
 					IADLXManualGraphicsTuning2Ptr ppManualGFXTuning;
 					adlx_Res0 = ppGPUTuningServices->GetManualGFXTuning(ppGpuInfo, (IADLXInterface**)&ppManualGFXTuning);
 
+					//Note: some gpus seem to return ADLX_NOT_SUPPORTED when reading frequency returning 0 or 1 values
 					adlx_Res0 = ppManualGFXTuning->GetGPUMinFrequencyRange(&adlx_IntRange0);
-					slider_Core_Min().Minimum(adlx_IntRange0.minValue);
-					slider_Core_Min().Maximum(adlx_IntRange0.maxValue);
-					slider_Core_Min().StepFrequency(adlx_IntRange0.step);
+					if (ADLX_SUCCEEDED(adlx_Res0))
+					{
+						slider_Core_Min().Minimum(adlx_IntRange0.minValue);
+						slider_Core_Min().Maximum(adlx_IntRange0.maxValue);
+						slider_Core_Min().StepFrequency(adlx_IntRange0.step);
 
-					adlx_Res0 = ppManualGFXTuning->GetGPUMinFrequency(&adlx_Int0);
-					slider_Core_Min().Value(adlx_Int0);
+						adlx_Res0 = ppManualGFXTuning->GetGPUMinFrequency(&adlx_Int0);
+						slider_Core_Min().Value(adlx_Int0);
+
+						//Enable or disable interface
+						slider_Core_Min().IsEnabled(true);
+					}
+					else
+					{
+						//Enable or disable interface
+						slider_Core_Min().IsEnabled(false);
+					}
 
 					adlx_Res0 = ppManualGFXTuning->GetGPUMaxFrequencyRange(&adlx_IntRange0);
-					slider_Core_Max().Minimum(adlx_IntRange0.minValue);
-					slider_Core_Max().Maximum(adlx_IntRange0.maxValue);
-					slider_Core_Max().StepFrequency(adlx_IntRange0.step);
+					if (ADLX_SUCCEEDED(adlx_Res0))
+					{
+						slider_Core_Max().Minimum(adlx_IntRange0.minValue);
+						slider_Core_Max().Maximum(adlx_IntRange0.maxValue);
+						slider_Core_Max().StepFrequency(adlx_IntRange0.step);
 
-					adlx_Res0 = ppManualGFXTuning->GetGPUMaxFrequency(&adlx_Int0);
-					slider_Core_Max().Value(adlx_Int0);
+						adlx_Res0 = ppManualGFXTuning->GetGPUMaxFrequency(&adlx_Int0);
+						slider_Core_Max().Value(adlx_Int0);
+
+						//Enable or disable interface
+						slider_Core_Max().IsEnabled(true);
+					}
+					else
+					{
+						//Enable or disable interface
+						slider_Core_Max().IsEnabled(false);
+					}
 
 					adlx_Res0 = ppManualGFXTuning->GetGPUVoltageRange(&adlx_IntRange0);
-					slider_Power_Voltage().Minimum(adlx_IntRange0.minValue);
-					slider_Power_Voltage().Maximum(adlx_IntRange0.maxValue);
-					slider_Power_Voltage().StepFrequency(adlx_IntRange0.step);
+					if (ADLX_SUCCEEDED(adlx_Res0))
+					{
+						slider_Power_Voltage().Minimum(adlx_IntRange0.minValue);
+						slider_Power_Voltage().Maximum(adlx_IntRange0.maxValue);
+						slider_Power_Voltage().StepFrequency(adlx_IntRange0.step);
 
-					adlx_Res0 = ppManualGFXTuning->GetGPUVoltage(&adlx_Int0);
-					slider_Power_Voltage().Value(adlx_Int0);
+						adlx_Res0 = ppManualGFXTuning->GetGPUVoltage(&adlx_Int0);
+						slider_Power_Voltage().Value(adlx_Int0);
 
-					//Enable or disable interface
-					slider_Core_Min().IsEnabled(true);
-					slider_Core_Max().IsEnabled(true);
-					slider_Power_Voltage().IsEnabled(true);
+						//Enable or disable interface
+						slider_Power_Voltage().IsEnabled(true);
+					}
+					else
+					{
+						//Enable or disable interface
+						slider_Power_Voltage().IsEnabled(false);
+					}
 				}
 				else
 				{
@@ -89,15 +118,23 @@ namespace winrt::RadeonTuner::implementation
 					adlx_Res0 = ppGPUTuningServices->GetManualVRAMTuning(ppGpuInfo, (IADLXInterface**)&ppManualVRAMTuning);
 
 					adlx_Res0 = ppManualVRAMTuning->GetMaxVRAMFrequencyRange(&adlx_IntRange0);
-					slider_Memory_Max().Minimum(adlx_IntRange0.minValue);
-					slider_Memory_Max().Maximum(adlx_IntRange0.maxValue);
-					slider_Memory_Max().StepFrequency(adlx_IntRange0.step);
+					if (ADLX_SUCCEEDED(adlx_Res0))
+					{
+						slider_Memory_Max().Minimum(adlx_IntRange0.minValue);
+						slider_Memory_Max().Maximum(adlx_IntRange0.maxValue);
+						slider_Memory_Max().StepFrequency(adlx_IntRange0.step);
 
-					adlx_Res0 = ppManualVRAMTuning->GetMaxVRAMFrequency(&adlx_Int0);
-					slider_Memory_Max().Value(adlx_Int0);
+						adlx_Res0 = ppManualVRAMTuning->GetMaxVRAMFrequency(&adlx_Int0);
+						slider_Memory_Max().Value(adlx_Int0);
 
-					//Enable or disable interface
-					slider_Memory_Max().IsEnabled(true);
+						//Enable or disable interface
+						slider_Memory_Max().IsEnabled(true);
+					}
+					else
+					{
+						//Enable or disable interface
+						slider_Memory_Max().IsEnabled(false);
+					}
 
 					//Get memory timing settting
 					adlx_Res0 = ppManualVRAMTuning->IsSupportedMemoryTiming(&adlx_Bool);
@@ -105,10 +142,18 @@ namespace winrt::RadeonTuner::implementation
 					{
 						ADLX_MEMORYTIMING_DESCRIPTION memoryTimingDescription;
 						adlx_Res0 = ppManualVRAMTuning->GetMemoryTimingDescription(&memoryTimingDescription);
-						combobox_Memory_Timing().SelectedIndex(memoryTimingDescription);
+						if (ADLX_SUCCEEDED(adlx_Res0))
+						{
+							combobox_Memory_Timing().SelectedIndex(memoryTimingDescription);
 
-						//Enable or disable interface
-						combobox_Memory_Timing().IsEnabled(true);
+							//Enable or disable interface
+							combobox_Memory_Timing().IsEnabled(true);
+						}
+						else
+						{
+							//Enable or disable interface
+							combobox_Memory_Timing().IsEnabled(false);
+						}
 					}
 					else
 					{
@@ -141,30 +186,46 @@ namespace winrt::RadeonTuner::implementation
 					adlx_Res0 = ppGPUTuningServices->GetManualPowerTuning(ppGpuInfo, (IADLXInterface**)&ppManualPowerTuning);
 
 					adlx_Res0 = ppManualPowerTuning->GetPowerLimitRange(&adlx_IntRange0);
-					slider_Power_Limit().Minimum(adlx_IntRange0.minValue);
-					slider_Power_Limit().Maximum(adlx_IntRange0.maxValue);
-					slider_Power_Limit().StepFrequency(adlx_IntRange0.step);
+					if (ADLX_SUCCEEDED(adlx_Res0))
+					{
+						slider_Power_Limit().Minimum(adlx_IntRange0.minValue);
+						slider_Power_Limit().Maximum(adlx_IntRange0.maxValue);
+						slider_Power_Limit().StepFrequency(adlx_IntRange0.step);
 
-					adlx_Res0 = ppManualPowerTuning->GetPowerLimit(&adlx_Int0);
-					slider_Power_Limit().Value(adlx_Int0);
+						adlx_Res0 = ppManualPowerTuning->GetPowerLimit(&adlx_Int0);
+						slider_Power_Limit().Value(adlx_Int0);
 
-					//Enable or disable interface
-					slider_Power_Limit().IsEnabled(true);
+						//Enable or disable interface
+						slider_Power_Limit().IsEnabled(true);
+					}
+					else
+					{
+						//Enable or disable interface
+						slider_Power_Limit().IsEnabled(false);
+					}
 
 					//Get power tdc settting
 					adlx_Res0 = ppManualPowerTuning->IsSupportedTDCLimit(&adlx_Bool);
 					if (adlx_Bool)
 					{
 						adlx_Res0 = ppManualPowerTuning->GetTDCLimitRange(&adlx_IntRange0);
-						slider_Power_TDC().Minimum(adlx_IntRange0.minValue);
-						slider_Power_TDC().Maximum(adlx_IntRange0.maxValue);
-						slider_Power_TDC().StepFrequency(adlx_IntRange0.step);
+						if (ADLX_SUCCEEDED(adlx_Res0))
+						{
+							slider_Power_TDC().Minimum(adlx_IntRange0.minValue);
+							slider_Power_TDC().Maximum(adlx_IntRange0.maxValue);
+							slider_Power_TDC().StepFrequency(adlx_IntRange0.step);
 
-						adlx_Res0 = ppManualPowerTuning->GetTDCLimit(&adlx_Int0);
-						slider_Power_TDC().Value(adlx_Int0);
+							adlx_Res0 = ppManualPowerTuning->GetTDCLimit(&adlx_Int0);
+							slider_Power_TDC().Value(adlx_Int0);
 
-						//Enable or disable interface
-						slider_Power_TDC().IsEnabled(true);
+							//Enable or disable interface
+							slider_Power_TDC().IsEnabled(true);
+						}
+						else
+						{
+							//Enable or disable interface
+							slider_Power_TDC().IsEnabled(false);
+						}
 					}
 					else
 					{
