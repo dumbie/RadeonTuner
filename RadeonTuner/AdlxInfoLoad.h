be8452695ee18cc0b14ba20c4bef1790ea31b0e4
Arnold Vink <info@arnoldvink.com>
2026-06-24 10:58:29 +0200

    - Read device information using ADL2 api.

--- RadeonTuner/AdlxInfoLoad.h
+++ RadeonTuner/AdlxInfoLoad.h
@@ -12,56 +12,47 @@ namespace winrt::RadeonTuner::implementation
 	{
 		try
 		{
-			//Fix missing Pixel and Texture Fillrate, Shaders Count, Bus Width, Fabrication Technology, Transistor Count, Revision.
+			//Fix missing Pixel and Texture Fillrate, Shaders Count, Bus Width, Fabrication Technology, Transistor Count, Resizable Bar.
 
 			//Set gpu information
 			std::wstring gpu_info = L"";
 
-			//Device name
-			try
+			//Get gpu information
+			std::optional<AdapterInfo> gpuInfo = AdlGetGpuByAdapterIndex(adl_Gpu_AdapterIndex);
+			if (gpuInfo.has_value())
 			{
-				const char* productName = NULL;
-				adlx_Res0 = ppGpuInfo->Name(&productName);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					gpu_info += L"Device name: " + char_to_wstring(productName);
-				}
+				AdapterInfo adapterInfo = gpuInfo.value();
+
+				//Device name
+				gpu_info += L"Device name: " + char_to_wstring(adapterInfo.strAdapterName);
+
+				//Device identifier
+				std::wstring device_id = char_to_wstring(adapterInfo.strPNPString);
+				device_id = wstring_get_between(device_id, L"\\", L"\\");
+				gpu_info += L"\nDevice identifier: " + device_id;
 			}
-			catch (...) {}
 
 			//Device codename
 			try
 			{
-				const char* codeName = NULL;
-				adlx_Res0 = ppGpuInfo->ProductName(&codeName);
-				if (ADLX_SUCCEEDED(adlx_Res0))
+				const int productNameSize = 32;
+				char productNameBuffer[productNameSize]{};
+				adl_Res0 = _ADL2_Adapter_ProductName_Get(adl_Context, adl_Gpu_AdapterIndex, productNameBuffer);
+				if (adl_Res0 == ADL_OK)
 				{
-					gpu_info += L"\nDevice codename: " + char_to_wstring(codeName);
+					gpu_info += L"\nDevice codename: " + char_to_wstring(productNameBuffer);
 				}
 			}
 			catch (...) {}
 
-			//Device identifier
-			std::wstring device_id = AdlxGetGpuIdentifier(ppGpuInfo);
-			if (!device_id.empty())
-			{
-				gpu_info += L"\nDevice identifier: " + device_id;
-			}
-
-			//Load driver version
-			//Note: this may cause ntdll crash that cannot be catched with old drivers
+			//Driver information
 			try
 			{
-				const char* driverVersion = NULL;
-				const char* driverWindows = NULL;
-				adlx_uint driverYear, driverMonth, driverDay;
-				adlx_Res0 = ppGpuInfo->AMDSoftwareVersion(&driverVersion);
-				adlx_Res0 = ppGpuInfo->AMDWindowsDriverVersion(&driverWindows);
-				adlx_Res0 = ppGpuInfo->AMDSoftwareReleaseDate(&driverYear, &driverMonth, &driverDay);
-				if (ADLX_SUCCEEDED(adlx_Res0))
+				ADLVersionsInfoX2 driverInfo{};
+				adl_Res0 = _ADL2_Graphics_VersionsX3_Get(adl_Context, adl_Gpu_AdapterIndex, &driverInfo);
+				if (adl_Res0 == ADL_OK)
 				{
-					std::wstring driverDate = number_to_wstring(driverYear) + L"/" + number_to_wstring(driverMonth) + L"/" + number_to_wstring(driverDay);
-					gpu_info += L"\nDriver version: " + char_to_wstring(driverVersion) + L" / " + char_to_wstring(driverWindows) + L" / " + driverDate;
+					gpu_info += L"\nDriver version: " + char_to_wstring(driverInfo.strCrimsonVersion) + L" / " + char_to_wstring(driverInfo.strDriverVer);
 				}
 			}
 			catch (...) {}
@@ -69,13 +60,11 @@ namespace winrt::RadeonTuner::implementation
 			//Bios information
 			try
 			{
-				const char* biosPart = NULL;
-				const char* biosVersion = NULL;
-				const char* biosDate = NULL;
-				adlx_Res0 = ppGpuInfo->BIOSInfo(&biosPart, &biosVersion, &biosDate);
-				if (ADLX_SUCCEEDED(adlx_Res0))
+				ADLBiosInfo biosInfo{};
+				adl_Res0 = _ADL2_Adapter_VideoBiosInfo_Get(adl_Context, adl_Gpu_AdapterIndex, &biosInfo);
+				if (adl_Res0 == ADL_OK)
 				{
-					gpu_info += L"\nBios version: " + char_to_wstring(biosPart) + L" / " + char_to_wstring(biosVersion) + L" / " + char_to_wstring(biosDate);
+					gpu_info += L"\nBios version: " + char_to_wstring(biosInfo.strPartNumber) + L" / " + char_to_wstring(biosInfo.strVersion) + L" / " + char_to_wstring(biosInfo.strDate);
 				}
 			}
 			catch (...) {}
@@ -83,28 +72,18 @@ namespace winrt::RadeonTuner::implementation
 			//Memory information
 			try
 			{
-				adlx_uint totalVRAM;
-				adlx_Res0 = ppGpuInfo->TotalVRAM(&totalVRAM);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					gpu_info += L"\nMemory details: " + number_to_wstring(totalVRAM) + L"MB ";
-				}
-
-				const char* typeVRAM = NULL;
-				adlx_Res0 = ppGpuInfo->VRAMType(&typeVRAM);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					gpu_info += char_to_wstring(typeVRAM);
-				}
-
 				ADLMemoryInfoX4 lpMemoryInfoX4{};
 				adl_Res0 = _ADL2_Adapter_MemoryInfoX4_Get(adl_Context, adl_Gpu_AdapterIndex, &lpMemoryInfoX4);
 				if (adl_Res0 == ADL_OK)
 				{
+					gpu_info += L"\nMemory details: " + number_to_wstring(lpMemoryInfoX4.iMemorySize / 1024 / 1024) + L"MB ";
+					gpu_info += char_to_wstring(lpMemoryInfoX4.strMemoryType);
+
 					if (lpMemoryInfoX4.iVramVendorRevId > 0)
 					{
 						gpu_info += L" (" + VramVendorNameFromId(lpMemoryInfoX4.iVramVendorRevId) + L")";
 					}
+
 					if (lpMemoryInfoX4.iMemoryBandwidth > 0)
 					{
 						gpu_info += L"\nMemory bandwidth: " + number_to_wstring(lpMemoryInfoX4.iMemoryBandwidth) + L"MB/s";
@@ -139,37 +118,17 @@ namespace winrt::RadeonTuner::implementation
 			//PCI Bus information
 			try
 			{
-				adlx_uint busLaneWidth = 0;
-				ADLX_PCI_BUS_TYPE busType = UNDEFINED;
-				adlx_Res0 = ppGpuInfo->PCIBusType(&busType);
-				if (ADLX_SUCCEEDED(adlx_Res0))
+				ADLChipSetInfo adlChipSetInfo{};
+				adl_Res0 = _ADL2_Adapter_ChipSetInfo_Get(adl_Context, adl_Gpu_AdapterIndex, &adlChipSetInfo);
+				if (adl_Res0 == ADL_OK)
 				{
-					gpu_info += L"\nBus type: " + ADLX_PCI_BUS_TYPE_STRING[busType];
-				}
-
-				adlx_Res0 = ppGpuInfo->PCIBusLaneWidth(&busLaneWidth);
-				if (ADLX_SUCCEEDED(adlx_Res0) && busLaneWidth > 0)
-				{
-					gpu_info += L" x" + number_to_wstring(busLaneWidth);
-				}
-			}
-			catch (...) {}
-
-			//Resizable BAR status
-			try
-			{
-				IADLXSmartAccessMemoryPtr ppSmartAccessMemory;
-				adlx_Res0 = ppGPUTuningServices->GetSmartAccessMemory(ppGpuInfo, &ppSmartAccessMemory);
-				if (ppSmartAccessMemory != NULL)
-				{
-					adlx_Res0 = ppSmartAccessMemory->IsEnabled(&adlx_Bool);
-					if (ADLX_SUCCEEDED(adlx_Res0) && adlx_Bool)
+					if (adlChipSetInfo.iBusType > 0)
 					{
-						gpu_info += L"\nResizable BAR / SAM: Enabled";
-					}
-					else
-					{
-						gpu_info += L"\nResizable BAR / SAM: Disabled";
+						gpu_info += L"\nBus type: " + ADLX_PCI_BUS_TYPE_STRING[adlChipSetInfo.iBusType];
+						if (adlChipSetInfo.iCurrentPCIELaneWidth > 0)
+						{
+							gpu_info += L" (x" + number_to_wstring(adlChipSetInfo.iCurrentPCIELaneWidth) + L" / x" + number_to_wstring(adlChipSetInfo.iMaxPCIELaneWidth) + L")";
+						}
 					}
 				}
 			}
@@ -186,119 +145,30 @@ namespace winrt::RadeonTuner::implementation
 	{
 		try
 		{
+			//Fix missing Current Resolution, FreeSync Range and HDR status, Refresh rate, Pixel clock
+
 			//Set display information
 			std::wstring display_info = L"";
 
-			//Display name
-			try
+			//Get connected displays
+			std::vector<ADLDisplayInfo> displayList = AdlGetDisplayAllByAdapterIndex(adl_Gpu_AdapterIndex);
+			for (ADLDisplayInfo displayInfo : displayList)
 			{
-				const char* displayName = NULL;
-				adlx_Res0 = ppDisplayInfo->Name(&displayName);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					display_info += L"Display name: " + char_to_wstring(displayName);
-				}
-			}
-			catch (...) {}
+				//Display name
+				display_info += L"Display name: " + char_to_wstring(displayInfo.strDisplayName);
 
-			//Native resolution
-			try
+				//Connector type
+				display_info += L" (" + ADLX_DISPLAY_CONNECTOR_TYPE_STRING[displayInfo.iDisplayConnector - 1] + L")\n";
+			}
+
+			//Check connected displays
+			if (displayList.empty())
 			{
-				adlx_int hResolution, vResolution;
-				adlx_Res0 = ppDisplayInfo->NativeResolution(&hResolution, &vResolution);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					if (hResolution > 0 && vResolution > 0)
-					{
-						display_info += L"\nNative resolution: " + number_to_wstring(hResolution) + L"x" + number_to_wstring(vResolution);
-					}
-				}
+				display_info = L"No connected displays on selected GPU.";
 			}
-			catch (...) {}
 
-			//Refresh rate
-			try
-			{
-				adlx_double refreshRate;
-				adlx_Res0 = ppDisplayInfo->RefreshRate(&refreshRate);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					if (refreshRate > 0)
-					{
-						display_info += L"\nRefresh rate: " + number_to_wstring((int)refreshRate) + L" Hz";
-					}
-				}
-			}
-			catch (...) {}
-
-			//Pixel clock
-			try
-			{
-				adlx_uint pixelClock;
-				adlx_Res0 = ppDisplayInfo->PixelClock(&pixelClock);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					if (pixelClock > 0)
-					{
-						display_info += L"\nPixel clock: " + number_to_wstring(pixelClock) + L" KHz";
-					}
-				}
-			}
-			catch (...) {}
-
-			//Connector type
-			try
-			{
-				ADLX_DISPLAY_CONNECTOR_TYPE connectType;
-				adlx_Res0 = ppDisplayInfo->ConnectorType(&connectType);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					display_info += L"\nConnector type: " + ADLX_DISPLAY_CONNECTOR_TYPE_STRING[connectType];
-				}
-			}
-			catch (...) {}
-
-			//Scan type
-			try
-			{
-				ADLX_DISPLAY_SCAN_TYPE scanType;
-				adlx_Res0 = ppDisplayInfo->ScanType(&scanType);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					display_info += L"\nScan type: " + ADLX_DISPLAY_SCAN_TYPE_STRING[scanType];
-				}
-			}
-			catch (...) {}
-
-			//DP link rate
-			try
-			{
-				IADLXDisplayConnectivityExperiencePtr ppDisplayConnectivityExperience;
-				adlx_Res0 = ppDispServices->GetDisplayConnectivityExperience(ppDisplayInfo, &ppDisplayConnectivityExperience);
-
-				ADLX_DP_LINK_RATE dpLinkRate;
-				adlx_Res0 = ppDisplayConnectivityExperience->GetDPLinkRate(&dpLinkRate);
-				if (ADLX_SUCCEEDED(adlx_Res0))
-				{
-					display_info += L"\nDP link rate: " + ADLX_DP_LINK_RATE_STRING[dpLinkRate];
-				}
-			}
-			catch (...) {}
-
-			//Link protection
-			//Note: this may cause ntdll crash that cannot be catched with old drivers
-			//try
-			//{
-			//	adlx_Res0 = ppDisplayConnectivityExperience->IsEnabledLinkProtection(&adlx_Bool);
-			//	if (ADLX_SUCCEEDED(adlx_Res0))
-			//	{
-			//		std::wstring linkProtectionString = adlx_Bool ? L"Disabled" : L"Enabled";
-			//		display_info += L"\nLink protection: " + linkProtectionString;
-			//	}
-			//}
-			//catch (...) {}
-
-			//Fix add current resolution, FreeSync range and HDR status
+			//Trim string ending
+			display_info = wstring_trim_right(display_info);
 
 			//Return information
 			return display_info;
@@ -311,11 +181,11 @@ namespace winrt::RadeonTuner::implementation
 	{
 		try
 		{
-			//Get versions
+			//Get version
 			std::wstring app_version = L"V" + string_to_wstring(GetVersionFromResource(AppVariables::hInstance));
-			std::wstring adlx_version = L" / ADLX " + char_to_wstring(ppADLXHelper.QueryVersion());
 
-			return L"Made by Arnold Vink\n" + app_version + adlx_version;
+			//Return version
+			return L"Made by Arnold Vink\n" + app_version;
 		}
 		catch (...) {}
 		return L"Failed to load application information.";
@@ -338,128 +208,5 @@ namespace winrt::RadeonTuner::implementation
 			//Set result
 			AVDebugWriteLine("ADLX failed loading information.");
 		}
-	}
-
-	std::pair<IADLXGPU2Ptr, int> MainPage::AdlxGetGpuPointer(std::string gpuIdentifier)
-	{
-		try
-		{
-			//Loop through all gpus
-			for (UINT i = 0; i < ppGpuList->Size(); i++)
-			{
-				try
-				{
-					//Get gpu pointer
-					IADLXGPU2Ptr gpuPointer;
-					adlx_Res0 = ppGpuList->At(i, (IADLXGPU**)&gpuPointer);
-
-					//Get gpu identifier
-					std::wstring device_id_w = AdlxGetGpuIdentifier(gpuPointer);
-					std::string device_id_a = wstring_to_string(device_id_w);
-
-					//Check gpu identifier
-					if (device_id_a == gpuIdentifier)
-					{
-						//Get ADL adapter index
-						int gpuAdapterIndex = -1;
-						adlx_Res0 = ppAdlMapping->AdlAdapterIndexFromADLXGPU(gpuPointer, &gpuAdapterIndex);
-
-						//Return result
-						return { gpuPointer, gpuAdapterIndex };
-					}
-				}
-				catch (...) {}
-			}
-		}
-		catch (...) {}
-		return { nullptr, -1 };
-	}
-
-	std::wstring MainPage::AdlxGetGpuIdentifier(IADLXGPU2Ptr gpuPointer)
-	{
-		std::wstring device_id = L"";
-		try
-		{
-			const char* vendorId = NULL;
-			adlx_Res0 = gpuPointer->VendorId(&vendorId);
-			if (vendorId != NULL)
-			{
-				device_id += char_to_wstring(vendorId);
-				device_id += L" ";
-			}
-
-			const char* deviceId = NULL;
-			adlx_Res0 = gpuPointer->DeviceId(&deviceId);
-			if (deviceId != NULL)
-			{
-				device_id += char_to_wstring(deviceId);
-				device_id += L" - ";
-			}
-
-			const char* subSystemVendorId = NULL;
-			adlx_Res0 = gpuPointer->SubSystemVendorId(&subSystemVendorId);
-			if (subSystemVendorId != NULL)
-			{
-				device_id += char_to_wstring(subSystemVendorId);
-				device_id += L" ";
-			}
-
-			const char* subSystemId = NULL;
-			adlx_Res0 = gpuPointer->SubSystemId(&subSystemId);
-			if (subSystemId != NULL)
-			{
-				device_id += char_to_wstring(subSystemId);
-				device_id += L" - ";
-			}
-
-			const char* revisionId = NULL;
-			adlx_Res0 = gpuPointer->RevisionId(&revisionId);
-			if (revisionId != NULL)
-			{
-				device_id += char_to_wstring(revisionId);
-				device_id += L" - ";
-			}
-
-			int uniqueId = -1;
-			adlx_Res0 = gpuPointer->UniqueId(&uniqueId);
-			if (uniqueId > 0)
-			{
-				device_id += number_to_wstring(uniqueId);
-			}
-		}
-		catch (...) {}
-		return device_id;
-	}
-
-	std::wstring MainPage::AdlxGetDisplayIdentifier(IADLXDisplayPtr displayPointer)
-	{
-		std::wstring device_id = L"";
-		try
-		{
-			uint32_t manufacturerId = NULL;
-			adlx_Res0 = displayPointer->ManufacturerID(&manufacturerId);
-			if (manufacturerId != NULL)
-			{
-				device_id += number_to_wstring(manufacturerId);
-				device_id += L" ";
-			}
-
-			const char* displayName = NULL;
-			adlx_Res0 = displayPointer->Name(&displayName);
-			if (displayName != NULL)
-			{
-				device_id += char_to_wstring(displayName);
-				device_id += L" ";
-			}
-
-			size_t uniqueId = -1;
-			adlx_Res0 = displayPointer->UniqueId(&uniqueId);
-			if (uniqueId > 0)
-			{
-				device_id += number_to_wstring(uniqueId);
-			}
-		}
-		catch (...) {}
-		return device_id;
 	}
 }
