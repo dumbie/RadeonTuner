433fc0f1232b6a9b30795fca6ab6ae6bc3e7d455
Arnold Vink <info@arnoldvink.com>
2026-08-26 21:19:09 +0200

    - Added Get function to AdlSetting structure.

--- RadeonTuner/AdlSettingsClass.h
+++ RadeonTuner/AdlSettingsClass.h
@@ -1,29 +1,50 @@
 #pragma once
 #include "pch.h"
 
-struct SettingInt
+enum class AdlSettingGet
+{
+	Current,
+	Default
+};
+
+struct AdlSettingInt
 {
 	std::optional<int> Current;
 	std::optional<int> Default;
-	std::optional<bool> Support;
 	std::optional<int> Minimum;
 	std::optional<int> Maximum;
 	std::optional<int> Step;
+	bool Support;
+
+	std::optional<int> Get(AdlSettingGet settingType)
+	{
+		return settingType == AdlSettingGet::Current ? Current : Default;
+	}
 };
 
-struct SettingFloat
+struct AdlSettingFloat
 {
 	std::optional<float> Current;
 	std::optional<float> Default;
-	std::optional<bool> Support;
 	std::optional<float> Minimum;
 	std::optional<float> Maximum;
 	std::optional<float> Step;
+	bool Support;
+
+	std::optional<float> Get(AdlSettingGet settingType)
+	{
+		return settingType == AdlSettingGet::Current ? Current : Default;
+	}
 };
 
-struct SettingString
+struct AdlSettingString
 {
 	std::optional<std::wstring> Current;
 	std::optional<std::wstring> Default;
-	std::optional<bool> Support;
+	bool Support;
+
+	std::optional<std::wstring> Get(AdlSettingGet settingType)
+	{
+		return settingType == AdlSettingGet::Current ? Current : Default;
+	}
 };
